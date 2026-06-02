3e5223a73b9f7db3913e05e22b7903c628176759
linkkokzel <linkkok7@gmail.com>
2026-06-02 12:11:07 +0800

    Add files via upload

---
+++ МДК.01.09 – Основы разработки приложений с использованием Windows Forms и WPF/Домашние задания/ВыДаНно 4.05.2026 Урок №15-16 знакомство с WPF/67/67/67/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using System;
+using System.Windows;
+
+namespace _67
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            double sum = 0;
+            double percent = 0;
+            double years = 0;
+
+            double.TryParse(tbSum.Text, out sum);
+            double.TryParse(tbPercent.Text, out percent);
+            double.TryParse(tbYears.Text, out years);
+
+            double total = sum + (sum * percent / 100 * years);
+
+            tbResult.Text =
+                "Итоговая сумма: " + total +
+                "\nПереплата: " + (total - sum);
+        }
+    }
+}
