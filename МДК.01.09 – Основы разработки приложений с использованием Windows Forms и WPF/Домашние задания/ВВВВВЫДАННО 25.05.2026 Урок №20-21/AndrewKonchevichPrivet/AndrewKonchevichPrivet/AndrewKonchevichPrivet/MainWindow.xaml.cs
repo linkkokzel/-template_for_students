8200a098055a1f9f71e4c86dc25116e7aa626775
linkkokzel <linkkok7@gmail.com>
2026-06-02 12:52:16 +0800

    Add files via upload

---
+++ МДК.01.09 – Основы разработки приложений с использованием Windows Forms и WPF/Домашние задания/ВВВВВЫДАННО 25.05.2026 Урок №20-21/AndrewKonchevichPrivet/AndrewKonchevichPrivet/AndrewKonchevichPrivet/MainWindow.xaml.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Collections.Generic;
+using System.Windows;
+// ✨ПИРОГ✨ это просто название приложения 
+// AndrewKonchevichPrivet это просто название данного пороекта 
+namespace AndrewKonchevichPrivet
+{
+    public partial class MainWindow : Window
+    {
+        List<string> titles = new List<string>();      
+        List<string> contents = new List<string>();    
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+       
+        private void AddBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+            if (TitleBox.Text == "")
+            {
+                MessageBox.Show("Введите заголовок!", "Ошибка");
+                return;
+            }
+
+            if (ContentBox.Text == "")
+            {
+                MessageBox.Show("Введите текст заметки!", "Ошибка");
+                return;
+            }
+
+            titles.Add(TitleBox.Text);
+            contents.Add(ContentBox.Text);
+
+            UpdateList();
+
+            ClearFields();
+
+            MessageBox.Show("Заметка добавлена!", "Готово");
+        }
+
+        private void ClearBtn_Click(object sender, RoutedEventArgs e)
+        {
+            ClearFields();
+        }
+
+        private void ClearFields()
+        {
+            TitleBox.Text = "";
+            ContentBox.Text = "";
+            TitleBox.Focus();
+        }
+
+        private void UpdateList()
+        {
+            NotesList.Items.Clear();
+            for (int i = 0; i < titles.Count; i++)
+            {
+                string preview = titles[i];
+                if (contents[i].Length > 30)
+                    preview += " - " + contents[i].Substring(0, 30) + "...";
+                else
+                    preview += " - " + contents[i];
+
+                NotesList.Items.Add(preview);
+            }
+        }
+
+        private void NotesList_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            int index = NotesList.SelectedIndex;
+            if (index >= 0 && index < titles.Count)
+            {
+                TitleBox.Text = titles[index];
+                ContentBox.Text = contents[index];
+            }
+        }
+    }
+}
